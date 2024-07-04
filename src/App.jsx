d004ef9792d76a4b03665147bb1bde19df9c12e7
Annaël Horthannice LALASON <140686269+Naelskrrrt@users.noreply.github.com>
2024-07-04 08:29:54 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="w-full flex items-center justify-center">
+      <p className="text-3xl font-bold text-blue-500"> Hello Project!</p>
+    </div>
+  )
+}
+
+export default App
